b60b8aebc4c9b76eecb277fe8186a5f2e52d6e0b
SimonThor12 <simon.lars.thor@gmail.com>
2024-08-09 12:33:14 +0200

    DaisyUI and tailwind added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center min-h-screen bg-black-100">
+        <h1 className="text-4xl font-bold text-center text-blue-500">
+          Time-to-Time
+        </h1>
+        <button className="btn btn-primary mt-4">Click me!</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
